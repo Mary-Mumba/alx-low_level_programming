fe8967bb88bb39fa7deb250a0504469eac00939c
Mary-Mumba <mary3.mumba@gmail.com>
2023-06-15 07:55:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - displays "Programming is like building a multilingual puzzle"
+ * Needs to be folloewd by a new line
+ * Should Return 0.
+ * /
+ 
+ int main(void)
+
+ {  
+       puts("\""Programming is like building a multilingual puzzle");
+
+       return (0);
+ }       
