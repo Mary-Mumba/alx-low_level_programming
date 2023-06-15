b51c4c9795961b09ba2c80d100d7f0741ab1c45f
Mary-Mumba <mary3.mumba@gmail.com>
2023-06-15 08:00:41 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
  * main - displays "Programming is like building a multilingual puzzle"
  * Needs to be folloewd by a new line
  * Should Return 0.
- * /
+ */
  
  int main(void)
 
  {  
        puts("\""Programming is like building a multilingual puzzle");
-
        return (0);
  }       
