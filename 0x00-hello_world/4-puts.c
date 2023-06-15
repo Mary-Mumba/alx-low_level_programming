8e36669e62cd1d45d2d5f3252af581066220e394
Mary-Mumba <mary3.mumba@gmail.com>
2023-06-15 10:29:27 -0700

    4 puts recreated file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - displays "Programming is like building a multilingual puzzle"
  * Needs to be folloewd by a new line
- * Should Return: 0 always
+ * Should Return 0
  */
- 
- int main(void)
 
- {  
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
- }       
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
+5-printf
