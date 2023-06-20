ccdf300bcdd3cdc467f6371197d495d4dd7d941b
Mary-Mumba <mary3.mumba@gmail.com>
2023-06-20 12:48:32 -0700

    edits

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,14 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-* followed by a new line
+* print_alphabet - print all alphabet in lowercase
 */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
+{
+char c;
 
-{
-char ch;
-int i;
-i = 0;
-while (i < 10)
-{
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
-}
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
 _putchar('\n');
-i++;
-}
 }
