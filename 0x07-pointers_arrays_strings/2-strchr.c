f0073c0a01bbfdb3f1fcb3f56f80bd0584cae093
Mary-Mumba <mary3.mumba@gmail.com>
2023-07-04 12:06:22 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strchr - Entry point
+* @s: input
+* @c: input
+* Return: Always 0 (Success)
+*/
+
+char *_strchr(char *s, char c)
+{
+int e = 0;
+
+for (; s[e] >= '\0'; e++)
+{
+if (s[e] == c)
+return (&s[e]);
+}
+return (0);
+}
